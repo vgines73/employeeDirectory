7d35afb6fe3d62f969692097e2f87f4f94a251a4
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2021-02-20 23:24:43 -0800

    added bootstrap and changed color of navbar

--- client/src/Components/Nav/Nav.js
+++ client/src/Components/Nav/Nav.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 const Nav = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <Link className="navbar-brand" to="/">
-          Breh
+          BREH
         </Link>
         <div>
           <ul className="navbar-nav">
