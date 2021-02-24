50528e3f58469c4cbcefb4bf1dab374a1115af8d
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2021-02-24 15:22:02 -0800

    issue with prettier resolved.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-    import { BrowserRouter as Router, Route } from 'react-router-dom';
-    import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';  
+import './App.css';  
 import Nav from './Components/Nav/Nav';
 import Wrapper from './Components/Wrapper/Wrapper';
 import Home from './Pages/Home';
